cdd2c4a339ee0c6d77a9c563f0b04434a75351ef
Akshay Gunjur Surya Prakash <gsakshay20002000@gmail.com>
2023-11-20 16:55:10 -0500

    useful comments and documentation

--- server.js
+++ server.js
@@ -1,36 +1,49 @@
 /** @format */
 
+// Importing necessary modules
 const http = require("http")
 const mongoose = require("mongoose")
 require("dotenv").config()
 
+// Importing the Express app
 const app = require("./src/app")
+
+// Importing necessary functions from models
 const { createBusinessTires } = require("./src/models/tires/tires.model")
 const {
 	initializeBusinessCounter,
 } = require("./src/models/counter/counter.model")
 
+// Setting up environment variables
 const PORT = process.env.PORT || 8000
 const MONGO_URL = process.env.MONGO_URL
 
+// Creating an HTTP server using Express app
 const server = http.createServer(app)
 
+// Establishing MongoDB connection
 mongoose.connection.once("open", () => {
 	console.log("MongoDB connection successful")
 })
 mongoose.connection.on("error", (err) => console.error(err))
 
+// Starting the server after database setup
 async function startServer() {
+	// Connecting to MongoDB
 	await mongoose.connect(MONGO_URL)
-	// Business logic handling to define our tires -> This can be handlled with controllers and admins further
-	tires = await createBusinessTires()
-	// console.log(tires) -> Can do something with the following information
+
+	// Creating business tires (can be further handled with controllers/admins)
+	const tires = await createBusinessTires()
 	console.log("Successfully created following tires for the Business: ")
 	tires?.map((tire) => console.log(tire.tirename))
-	// Business logic of handling a main counter
+
+	// Initializing the main counter for business logic
 	const counter = await initializeBusinessCounter()
 	console.log("Main counter created successfully: ", counter.name)
+
+	// Starting the server to listen on defined PORT
 	server.listen(PORT, () => console.log(`Server is listening on ${PORT}`))
 }
 
+// Calling the startServer function to begin the server setup
 startServer()
--- src/models/urls/urls.model.js
+++ src/models/urls/urls.model.js
@@ -1,35 +1,60 @@
 /** @format */
 
-const URL = require("./urls.mongo")
+// Importing the URL model
+const URL = require("./urls.model")
 
+/**
+ * Create a new URL entry.
+ * @param {Object} urlData - Data to create a URL entry (longUrl, shortUrl, user, accessCount).
+ * @returns {Promise<Object>} Newly created URL entry.
+ * @throws {Error} If an error occurs while creating the URL.
+ */
 async function createURL(urlData) {
 	try {
 		const newURL = new URL(urlData)
 		const savedURL = await newURL.save()
 		return savedURL
 	} catch (error) {
-		console.log(err)
 		throw new Error(`Error creating URL: ${error.message}`)
 	}
 }
 
+/**
+ * Retrieve URL entry by short URL.
+ * @param {string} shortUrl - Short URL of the entry.
+ * @returns {Promise<Object | null>} Retrieved URL entry or null if not found.
+ * @throws {Error} If an error occurs while fetching the URL through Short URL.
+ */
 async function getURLByShortUrl(shortUrl) {
 	try {
 		const url = await URL.findOne({ shortUrl })
 		return url
 	} catch (error) {
-		throw new Error(`Error fetching URL thourgh Short URL: ${error.message}`)
+		throw new Error(`Error fetching URL through Short URL: ${error.message}`)
 	}
 }
 
+/**
+ * Retrieve URL entry by long URL.
+ * @param {string} longUrl - Long URL of the entry.
+ * @returns {Promise<Object | null>} Retrieved URL entry or null if not found.
+ * @throws {Error} If an error occurs while fetching the URL through Long URL.
+ */
 async function getURLByLongUrl(longUrl) {
 	try {
 		const url = await URL.findOne({ longUrl })
 		return url
 	} catch (error) {
-		throw new Error(`Error fetching URL thorugh Long URL: ${error.message}`)
+		throw new Error(`Error fetching URL through Long URL: ${error.message}`)
 	}
 }
+
+/**
+ * Retrieve URLs associated with a specific user.
+ * @param {string} userId - ID of the user.
+ * @returns {Promise<Array>} List of URL entries associated with the user.
+ * @throws {Error} If an error occurs while fetching user URLs.
+ */
 async function getUrlsByUserId(userId) {
 	try {
 		const userUrls = await URL.find({ user: userId })
@@ -39,6 +64,12 @@ async function getUrlsByUserId(userId) {
 	}
 }
 
+/**
+ * Increment access count for a specific URL entry.
+ * @param {string} urlId - ID of the URL entry.
+ * @returns {Promise<Object>} Updated URL entry.
+ * @throws {Error} If an error occurs while incrementing the access count.
+ */
 async function incrementAccessCount(urlId) {
 	try {
 		const updatedUrl = await URL.findByIdAndUpdate(
@@ -47,7 +78,7 @@ async function incrementAccessCount(urlId) {
 			{ new: true }
 		)
 		return updatedUrl
-	} catch {
+	} catch (error) {
 		throw new Error(`Error incrementing the access count: ${error.message}`)
 	}
 }
